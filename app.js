96922812cb28db411913407eac44d1aa862d7a52
Heidi Cao <heidihcao@gmail.com>
2021-10-13 21:08:03 -0700

    seattle store completed

--- app.js
+++ app.js
@@ -3,7 +3,8 @@
 
 // object for my Seattle Store
 const seattleStore = document.getElementById('seattleList');
-let hours = ['6 a.m.', '7 a.m.', '8 a.m.', '9 a.m.', '10 a.m.', '11 a.m.','12 p.m.','1 p.m.','2 p.m.','3 p.m.','4 p.m.','5 p.m.','6 p.m.','7 p.m.'];
+let hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
+console.log(seattleStore);
 
 //  min 6
 // max 15
@@ -24,33 +25,41 @@ let seattle = {
   avg: 6.3,
   cookiesSoldEachHourArray: [],
   // between min and max * avg
-  getRandomCustomers: function() {
+  getRandomCustomers: function () {
     return Math.floor(Math.random() * (this.max - this.min + 1) + this.min);
   },
 
+  getCookieSalesPerHour: function () {
 
-  getCookieSalesPerHour: function() {
-    
     // write a for loop that uses the length of the hours away in that loop
-    for (let i=0; i<hours.length; i++){
-       
-    // Get the number of customers this hour
-    let customersThisHour = this.getRandomCustomers();
+    for (let i = 0; i < hours.length; i++) {
 
-    // calculate the cookies sold for all 14 hours
-    let totalCookiesSoldThisHour = Math.ceil(customersThisHour * this.avg);
-    
-    // that value of total cookies sold that hour on to our array
-    this.cookiesSoldEachHourArray.push(totalCookiesSoldThisHour);
+      // Get the number of customers this hour
+      let customersThisHour = this.getRandomCustomers();
+
+      // calculate the cookies sold for all 14 hours
+      let totalCookiesSoldThisHour = Math.ceil(customersThisHour * this.avg);
+
+      // that value of total cookies sold that hour on to our array
+      this.cookiesSoldEachHourArray.push(totalCookiesSoldThisHour);
+    }
     console.log(this.cookiesSoldEachHourArray);
+  },
 
-  }
-
-  renderTheList: function() {
+  renderTheList: function () {
     this.getCookieSalesPerHour();
     // code that will render the lists to the page
+    let p = document.createElement('p');
+    p.textContent = this.name;
+    seattleStore.appendChild(p);
+
+    for (let i = 0; i < this.cookiesSoldEachHourArray.length; i++) {
+      let li = document.createElement('li');
+      li.textContent = `${hours[i]}: ${this.cookiesSoldEachHourArray[i]} cookies`;
+      seattleStore.appendChild(li);
+    }
     // render the LIs inside our UL
   }
-},
+};
 
-seattle.renderTheList(),
+seattle.renderTheList();
